e10ffdf69ea71cc9f394d5a0b179a3789d680333
Nogmog <61653934+Nogmog@users.noreply.github.com>
2024-01-31 03:07:06 +0000

    initial commit

---
+++ functions/api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({"Status": "Online"});
+});
+
+app.use("/", router);
+module.exports.handler = serverless(app);
