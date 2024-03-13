d259167d30376c6ca8d611023eaab7676d61c51e
Nogmog <61653934+Nogmog@users.noreply.github.com>
2024-03-13 03:23:10 +0000

    Temporarily reverted new db migration

--- db.js
+++ db.js
@@ -1,31 +1,34 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: process.env.DATABASE_URL,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    port: 6306,
-    // connectionLimit: 10
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
+// const connection = mysql.createConnection({
+//     host: process.env.DATABASE_URL,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     port: 6306,
+//     // connectionLimit: 10
+// });
 
-// pool.getConnection(function(err, connection){
-//     if(err) console.log("POOL ERR:", err)
+// // pool.getConnection(function(err, connection){
+// //     if(err) console.log("POOL ERR:", err)
 
-//     connection.changeUser({database : "moistste"}, function(err){
-//         if(err) console.log("CHANGE USER ERR:", err)
-//     })
+// //     connection.changeUser({database : "moistste"}, function(err){
+// //         if(err) console.log("CHANGE USER ERR:", err)
+// //     })
 
+// // })
+
+// connection.connect(function(err){
+//     if(err) console.log("CONNECTION ERR:",err)
+//     console.log("Connected to database");
 // })
 
-connection.connect(function(err){
-    if(err) console.log("CONNECTION ERR:",err)
-    console.log("Connected to database");
-})
-
-connection.changeUser({database : "moistste"}, function(err){
-    if(err) console.log("CHANGE USER ERR:", err)
-})
+// connection.changeUser({database : "moistste"}, function(err){
+//     if(err) console.log("CHANGE USER ERR:", err)
+// })
 
 
-module.exports = connection;
+
+
+module.exports = pool;
