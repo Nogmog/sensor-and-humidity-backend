27658f4b0c5f339042cef2b0367555aeeee62af5
Nogmog <61653934+Nogmog@users.noreply.github.com>
2024-03-17 03:37:47 +0000

    previous was a mistake

--- src/routes/mcu.routes.js
+++ src/routes/mcu.routes.js
@@ -13,7 +13,7 @@ router.route("/modules/add")
     .post(auth.loggedInAuth, mcu.addNewDevice)
 
 router.route("/modules/:id")
-    .get(auth.loggedInAuth, mcu.getInformationByMac);
+    .get(auth.loggedInAuth, mcu.getMCUInformationByMac);
 
     
 module.exports = router;
