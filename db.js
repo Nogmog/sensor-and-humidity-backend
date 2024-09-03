cd28a37685fd701f45243ee8367fc4ecad2dcfbf
Nogmog <61653934+Nogmog@users.noreply.github.com>
2024-09-03 04:39:14 +0100

    Revert "Changes to work with Azure DB"
    This reverts commit 46cd46056b297120d60e852c2ad04ca7629efc58.

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 const mysql = require('mysql2');
-const fs = require("fs")
-
 
 const pool = mysql.createPool({
     host: process.env.DATABASE_URL,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306,
+    port: 6306,
     connectionLimit: 10,
     multipleStatements: true,
     ssl: {
-        ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")
+        ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem"),
+        rejectUnauthorized: false
     }
 });
 
